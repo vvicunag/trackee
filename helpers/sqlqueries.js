94318ebcc3a0d3f69105e9b818ddbf4234f01d73
vvicunag <vvicuna1@uc.cl>
2022-01-05 20:28:09 +0100

    add role questions/add pass name for value in inquirer

--- helpers/sqlqueries.js
+++ helpers/sqlqueries.js
@@ -1,29 +1,48 @@
-let conn
+let conn;
 const mysql = require("mysql2/promise");
-( async () => {
-conn = await mysql.createConnection({
+const initdb = async () => {
+  conn = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "cocodrilo69",
-    database: "trackee_db"
-});
-}) ();
+    database: "trackee_db",
+  });
+};
 
-const getEmployees =  () => {
-    const result = conn.execute("SELECT * FROM employees");
-    return result;
-}
+const getEmployees = () => {
+  const result = conn.execute("SELECT * FROM employees");
+  return result;
+};
 
 const getRoles = () => {
-    return conn.execute("SELECT * FROM roles");
-}
+  return conn.execute("SELECT * FROM roles");
+};
 
 const getDepartments = () => {
-    return conn.execute("SELECT * FROM departments")
-}
+  return conn.execute("SELECT * FROM departments");
+};
+
+const addSelectedDepartment = (response) => {
+  conn.execute(
+    `INSERT INTO departments (dep_name) VALUES ("${response.department}")`
+  );
+};
+
+const addSelectedRole = (response) => {
+  conn.execute(
+    `INSERT INTO roles (title, salary, department_id) VALUES (
+        "${response.title}", 
+        ${response.salary}, 
+        ${response.department}
+        )`
+  );
+};
 
 module.exports = {
-    getEmployees,
-    getRoles,
-    getDepartments   
-}
+  getEmployees,
+  getRoles,
+  getDepartments,
+  initdb,
+  addSelectedDepartment,
+  addSelectedRole,
+};
--- index.js
+++ index.js
@@ -1,84 +1,143 @@
-const inquirer = require("inquirer"); 
-const cTable = require('console.table');
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 const queries = require("./helpers/sqlqueries");
 
 // Question bank
-const initializeQuestion = 
-[
-    {
-        type: 'list',
-        message: 'What do you want to do?',
-        choices: ['See employees', 'See roles','See departments', 'Add employee', 'Quit'],
-        name: 'initialize',
-    },  
-]
-
+const initializeQuestion = [
+  {
+    type: "list",
+    message: "What do you want to do?",
+    choices: [
+      "See employees",
+      "See roles",
+      "See departments",
+      "Add employee",
+      "Add department",
+      "Add role",
+      "Quit",
+    ],
+    name: "initialize",
+  },
+];
 
 const createEmployeeQuestions = async () => {
   const roles = await getAllRoles();
   return [
-  {
-      type: 'input',
+    {
+      type: "input",
       message: "What is the employee's first name?",
-      name: 'first-name',
-  },
-  {
-    type: 'input',
-    message: "What is the employee's last name?",
-    name: 'last-name',
-  },  
-  {
-    type: 'input',
-    message: "What is the employee's last name?",
-    name: 'last-name',
-  },
-  {
-    type: 'list',
-    message: "What is the role of the employee?",
-    choices: roles.map(({title}) => title),
-    name: 'roles',
-  },
-]
+      name: "first-name",
+    },
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "last-name",
+    },
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "last-name",
+    },
+    {
+      type: "list",
+      message: "What is the role of the employee?",
+      choices: roles.map(({ title }) => title),
+      name: "roles",
+    },
+  ];
 };
 
-initializeApp = () => {
-    inquirer.prompt(initializeQuestion).then( async (response) => {
-        console.log(response);
-        if(response.initialize == "See employees") {
-            const [employees] = await queries.getEmployees();
-            console.table(employees);
-            initializeApp();
-        };
-        if(response.initialize == "See roles") {
-          const roles = await getAllRoles();
-          console.table(roles);
-          initializeApp();
-        };
-        if(response.initialize == "See departments") {
-          const [departments] = await queries.getDepartments();
-          console.table(departments);
-          initializeApp();
-        };
-        if(response.initialize == "Add employee") {
-          addEmployee();
-        };
-    });
+const createRoleQuestions = async () => {
+  const departments = await getAllDepartments();
+  return [
+    {
+      type: "input",
+      message: "What is the title of the role?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is the salary of the role?",
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "What is the department of the role?",
+      choices: departments.map(({ dep_name, id }) => {
+        return { name: dep_name, value: id };
+      }),
+      name: "department",
+    },
+  ];
 };
- 
+
+const runApp = async () => {
+  const response = await inquirer.prompt(initializeQuestion);
+
+  console.log(response);
+  if (response.initialize == "See employees") {
+    const [employees] = await queries.getEmployees();
+    console.table(employees);
+    runApp();
+  }
+  if (response.initialize == "See roles") {
+    const roles = await getAllRoles();
+    console.table(roles);
+    runApp();
+  }
+  if (response.initialize == "See departments") {
+    const departments = await getAllDepartments();
+    console.table(departments);
+    runApp();
+  }
+  if (response.initialize == "Add employee") {
+    addEmployee();
+  }
+  if (response.initialize == "Add department") {
+    addDepartment();
+  }
+  if (response.initialize == "Add role") {
+    addRole();
+  }
+};
 
 const getAllRoles = async () => {
   const [roles] = await queries.getRoles();
   return roles;
-}
+};
+
+const getAllDepartments = async () => {
+  const [departments] = await queries.getDepartments();
+  return departments;
+};
 
 const addEmployee = async () => {
   inquirer.prompt(await createEmployeeQuestions()).then((response) => {
     console.log(response);
   });
-}
+};
 
+const addDepartment = async () => {
+  await inquirer
+    .prompt({
+      type: "input",
+      message: "What is the name of the department?",
+      name: "department",
+    })
+    .then((response) => {
+      queries.addSelectedDepartment(response);
+    });
+};
 
+const addRole = async () => {
+  inquirer.prompt(await createRoleQuestions()).then((response) => {
+    queries.addSelectedRole(response);
+  });
+};
 
-initializeApp();
+const intializeApp = async () => {
+  await queries.initdb();
+  runApp();
+};
 
-
+intializeApp();
